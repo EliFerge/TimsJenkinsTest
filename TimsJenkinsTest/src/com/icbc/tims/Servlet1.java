792ec3618ffb7b40b29a8edd396b3b55fd7e26c4
weizheng <wz15614040345@163.com>
2021-01-14 20:51:39 +0800

    上传项目

---
+++ TimsJenkinsTest/src/com/icbc/tims/Servlet1.java
@@ -0,0 +1,26 @@
+package com.icbc.tims;
+
+import cn.hutool.core.date.DateUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/test1")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        String dateStr = "2017-03-01";
+        Date date = DateUtil.parse(dateStr);
+        String format = DateUtil.format(date, "yyyy/MM/dd");
+        response.getWriter().write(format);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
