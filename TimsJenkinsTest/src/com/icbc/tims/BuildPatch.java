509020c6ff6b3740337c7c24b5c3b476e10fa030
weizheng <wz15614040345@163.com>
2021-01-31 01:15:41 +0800

    增量试验

--- TimsJenkinsTest/src/com/icbc/tims/BuildPatch.java
+++ TimsJenkinsTest/src/com/icbc/tims/BuildPatch.java
@@ -16,16 +16,32 @@ public class BuildPatch {
         String new_version = versions.get(0).trim();
         String last_version = versions.get(1).trim();
         String base_Version = FileUtil.readLines(FileUtil.getWebRoot() + File.separator + "tims_base_version.txt", "utf-8").get(0);
-        if (StrUtil.hasEmpty(base_Version)) {
+        String diff_cmd = "git diff " + new_version + " " + last_version + " --name-status";
+        if (!StrUtil.hasEmpty(base_Version)) {
             //获取最新版本号和前一次版本号差异
-            String diff_cmd = "git diff "+new_version+" "+last_version+" --name-status";
-            List<String> changelogs = RuntimeUtil.execForLines(diff_cmd);
-            List<String> d = changelogs.stream().filter(s -> s.trim().startsWith("D")).collect(Collectors.toList());
-            System.out.println(d);
-        }else{
-            //获取最新和基线差异
-            String diff_cmd = "git diff "+new_version+" "+base_Version+" --name-status";
+             diff_cmd = "git diff " + new_version + " " + base_Version + " --name-status";
+
         }
+        List<String> changelogs = RuntimeUtil.execForLines(diff_cmd);
+        List<String> d = changelogs.stream().filter(s -> s.trim().startsWith("A") || s.trim().startsWith("M"))
+                .map(s -> s.substring(1).trim()).collect(Collectors.toList());
+        System.out.println(d);
+        for (int i = 0; i < d.size(); i++) {
+            String pattern = "^.+/(?:src)/(.+)\\.java\\s*$";
+
+//                if (d.get(i).endsWith(".java") && d.get(i).startsWith("TimsJenkinsTest/src")) {
+//                    d.set(i,d.get(i).replace(".java", "*.class").replace("TimsJenkinsTest/src", "TimsJenkinsTest/WebContent/WEB-INF/classes"));
+//                }
+
+            if (d.get(i).matches(pattern)) {
+                d.set(i, d.get(i).replace(".java", "*.class").replace("TimsJenkinsTest/src", "TimsJenkinsTest/WebContent/WEB-INF/classes"));
+
+            }
+            d.set(i, d.get(i) + "*");
+            d.set(i, d.get(i).substring(15));
+        }
+        System.out.println(d);
+        FileUtil.writeLines(d, FileUtil.getWebRoot() + File.separator + "tims_patch.txt", "utf-8");
 
 
     }
