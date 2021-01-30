b78298b165e0ed4e41327447896f26d49cdad1e5
weizheng <wz15614040345@163.com>
2021-01-31 01:41:50 +0800

    增量试验

--- TimsJenkinsTest/src/com/icbc/tims/BuildPatch.java
+++ TimsJenkinsTest/src/com/icbc/tims/BuildPatch.java
@@ -15,6 +15,8 @@ public class BuildPatch {
         List<String> versions = RuntimeUtil.execForLines(versino_cmd);
         String new_version = versions.get(0).trim();
         String last_version = versions.get(1).trim();
+        System.out.println("最新版本号:"+new_version);
+        System.out.println("最新的前一次版本号:"+last_version);
         String base_Version = FileUtil.readLines(FileUtil.getWebRoot() + File.separator + "tims_base_version.txt", "utf-8").get(0);
         String diff_cmd = "git diff " + new_version + " " + last_version + " --name-status";
         if (!StrUtil.hasEmpty(base_Version)) {
@@ -25,7 +27,7 @@ public class BuildPatch {
         List<String> changelogs = RuntimeUtil.execForLines(diff_cmd);
         List<String> d = changelogs.stream().filter(s -> s.trim().startsWith("A") || s.trim().startsWith("M"))
                 .map(s -> s.substring(1).trim()).collect(Collectors.toList());
-        System.out.println(d);
+        System.out.println("所有AM文件列表:"+d);
         for (int i = 0; i < d.size(); i++) {
             String pattern = "^.+/(?:src)/(.+)\\.java\\s*$";
 
@@ -40,7 +42,7 @@ public class BuildPatch {
             d.set(i, d.get(i) + "*");
             d.set(i, d.get(i).substring(15));
         }
-        System.out.println(d);
+        System.out.println("patch文件列表:"+d);
         FileUtil.writeLines(d, FileUtil.getWebRoot() + File.separator + "tims_patch.txt", "utf-8");
 
 
