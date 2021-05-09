6666fd83dbb04b3579336ad7b0bab993a44dc8ea
weizheng <wz15614040345@163.com>
2021-05-09 11:06:58 +0800

    增量试验

--- TimsJenkinsTest/src/com/icbc/tims/Servlet1.java
+++ TimsJenkinsTest/src/com/icbc/tims/Servlet1.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @WebServlet("/test1")
 public class Servlet1 extends HttpServlet {
@@ -17,7 +19,17 @@ public class Servlet1 extends HttpServlet {
         String dateStr = "4444-05-05";
         Date date = DateUtil.parse(dateStr);
         String format = DateUtil.format(date, "yyyy/MM/dd");
+
+        new Thread(() -> {
+            List<String> list = new ArrayList<>();
+            while (true) {
+                list.add(new String("你弄啊是个浓缩的内功的giaso"));
+            }
+        }).start();
+
         response.getWriter().write(format);
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
